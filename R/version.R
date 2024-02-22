00bd23b87555f2fe929dbf76ed58c6fd675061c4
hubert-crea <hubert@energyandcleanair.org>
2024-02-22 14:04:47 +0800

    -adding export_versions fn

---
+++ R/version.R
@@ -0,0 +1,11 @@
+export_versions <- function(filepath="versions.csv", pattern="crea") {
+  # Export version of packages being used
+  ns <- loadedNamespaces()
+  ns <- ns[grepl(pattern, ns, ignore.case=T)]
+  ns %>%
+    lapply(function(x) {
+      tibble(package=x, version=packageVersion(x))
+    }) %>%
+    bind_rows() %>%
+    write_csv(filepath)
+}
